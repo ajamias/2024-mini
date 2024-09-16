7484d49239ea01c590ac7232abc89898a217aa7a
ajamias <ajamias@bu.edu>
2024-09-16 16:23:06 -0400

    Complete Exercise 2

--- assignment/exercise_sound.py
+++ assignment/exercise_sound.py
@@ -7,33 +7,45 @@ based on https://www.coderdojotc.org/micropython/sound/04-play-scale/
 
 import machine
 import utime
+import _thread
+
 
 # GP16 is the speaker pin
 SPEAKER_PIN = 16
 
 # create a Pulse Width Modulation Object on this pin
 speaker = machine.PWM(machine.Pin(SPEAKER_PIN))
+speaker.duty_u16(32000)
+
+done = False
+
+def wait(duration: float) -> None:
+    global done
+    utime.sleep_ms(duration)
+    done = True
+    _thread.exit()
 
 
-def playtone(frequency: float, duration: float) -> None:
-    speaker.duty_u16(1000)
-    speaker.freq(frequency)
-    utime.sleep(duration)
-
-
-def quiet():
-    speaker.duty_u16(0)
-
-
-freq: float = 30
-duration: float = 0.1  # seconds
-
-print("Playing frequency (Hz):")
-
-for i in range(64):
-    print(freq)
-    playtone(freq, duration)
-    freq = int(freq * 1.1)
+""" Travis Scott- FE!N """
+while True:
+    done = False
+    utime.sleep_ms(1)
+    _thread.start_new_thread(wait, (1500,))
+    
+    while not done:
+        speaker.freq(311)
+        speaker.freq(370)
+        speaker.freq(466)
+        
+    done = False
+    utime.sleep_ms(1)
+    _thread.start_new_thread(wait, (2000,))
+    
+    while not done:
+        speaker.freq(247)
+        speaker.freq(311)
+        speaker.freq(350)
+        speaker.freq(466)
 
 # Turn off the PWM
-quiet()
+speaker.duty_u16(0)
